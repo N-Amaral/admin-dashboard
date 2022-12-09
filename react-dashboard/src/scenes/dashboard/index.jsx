49cd4f946488c5db652976d05a65e37c75de8a6e
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2022-12-09 19:13:33 +0000

    Last commit, Finished dashboard comp.

--- react-dashboard/src/scenes/dashboard/index.jsx
+++ react-dashboard/src/scenes/dashboard/index.jsx
@@ -53,7 +53,6 @@ const Dashboard = () => {
     <Box m="20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
-
         <Box>
           <Button
             sx={{
@@ -140,8 +139,32 @@ const Dashboard = () => {
         {/* ROW 3 */}
 
         <Box gridColumn="span 4" gridRow="span 2" backgroundColor={colors.primary[400]} p="30px">
-          <Typography variant="h5" fontWeight="600"></Typography>
-          <Box display="flex" flexDirection="column" alignItems="center" mt="25px"></Box>
+          <Typography variant="h5" fontWeight="600">
+            Campaign
+          </Typography>
+          <Box display="flex" flexDirection="column" alignItems="center" mt="25px">
+            <ProgressCircle size="125" />
+            <Typography variant="h5" color={colors.greenAccent[500]} sx={{ mt: "15px" }}>
+              $40,000 in revenue
+            </Typography>
+            <Typography>Includes operational costs</Typography>
+          </Box>
+        </Box>
+        <Box gridColumn="span 4" gridRow="span 2" backgroundColor={colors.primary[400]}>
+          <Typography variant="h5" color={colors.greenAccent[500]} fontWeight="600" sx={{ padding: "30px 30px 0 30px" }}>
+            Sales Quantity
+          </Typography>
+          <Box height="250px" mt="-20px">
+            <BarChart isDashboard={true} />
+          </Box>
+        </Box>
+        <Box gridColumn="span 4" gridRow="span 2" backgroundColor={colors.primary[400]} p="30px">
+          <Typography variant="h5" color={colors.greenAccent[500]} sx={{ mb: "15px" }}>
+            Global Traffic Stats
+          </Typography>
+          <Box height="200px">
+            <GeographyChart isDashboard={true} />
+          </Box>
         </Box>
       </Box>
     </Box>
